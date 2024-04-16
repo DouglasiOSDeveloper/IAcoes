5cdb39417f1e6679c6628d76bb6541d4aef4a3db
Douglas Figueirôa <62218512+Douglas-euzinho@users.noreply.github.com>
2024-04-15 22:57:28 -0300

    feature/LSTM_Implementado

--- IAcoes.py
+++ IAcoes.py
@@ -1,188 +1,134 @@
-import yfinance as yf
-import matplotlib.pyplot as plt
 import pandas as pd
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-import itertools
-import warnings
 import numpy as np
+import yfinance as yf
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.preprocessing import MinMaxScaler
+from statsmodels.tsa.seasonal import seasonal_decompose
+from pmdarima import auto_arima
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, InputLayer
+from matplotlib.dates import DateFormatter
 
-# Definir o ticker da ação
-ticker = 'AAPL'
 
-# Obter os dados
-data = yf.download(ticker)
+#----------------------------INÍCIO - Implementação Modelo LSTM----------------------------#
+# Baixando dados históricos
+today = pd.Timestamp.now()
+tickers = ['PETR4.SA', 'CL=F']
+data = yf.download(tickers, start=today - pd.DateOffset(years=5), end=today)['Close']
+data = data.resample('M').last().dropna()  # Usando o último preço de cada mês
 
-# Selecionar dados a partir de 2020
-data_2020 = data['2020-01-01':]
+#----------------------------INÍCIO - Implementação da Decomposição de Séries Temporais (Ruídos)----------------------------#
+# Escolhendo uma coluna para decomposição, e.g., 'PETR4.SA'
+series = data['PETR4.SA']
+# Decomposição de séries temporais
+result = seasonal_decompose(series, model='additive', period=12)  # Supondo que 'M' representa mensal e um ciclo anual
+# Plotando os componentes
+decomposed = result.plot()
+plt.show()
+#----------------------------FIM - Implementação da Decomposição de Séries Temporais (Ruídos)----------------------------#
 
-# Ajustar o modelo ARIMA(2, 1, 2) aos seus dados
-model = ARIMA(data_2020['Close'], order=(2, 1, 2))
-results = model.fit()
+# Verificar os dados
+print(data.head())  # Isso ajudará a confirmar que estamos acessando os dados corretamente
 
-# Prever os próximos 30 dias com base nos valores ajustados
-forecast_values = results.forecast(steps=30)
+# Normalização dos dados
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data[['PETR4.SA', 'CL=F']])
 
-# Criar datas para os próximos 30 dias
-last_date = data_2020.index[-1]
-next_dates = pd.date_range(start=last_date, periods=31)  # Ignorar o último dia dos dados históricos
+# Função para criar o conjunto de dados
+def create_dataset(dataset, look_back=1):
+    X, Y = [], []
+    for i in range(len(dataset) - look_back - 1):
+        a = dataset[i:(i + look_back), :]
+        X.append(a)
+        Y.append(dataset[i + look_back, 0])  # Previsão do preço da PETR4
+    return np.array(X), np.array(Y)
 
-# Concatenar as séries de valores ajustados e previsões
-extended_values = pd.concat([results.fittedvalues, forecast_values])
+look_back = 3
+X, y = create_dataset(scaled_data, look_back)
+X = np.reshape(X, (X.shape[0], X.shape[1], 2))  # Ajustando a quantidade de features
 
-# Plotar os dados originais e as previsões
+#----------------------------INÍCIO - Validação Cruzada em Séries Temporais----------------------------#
+n_splits = 5  # Número de dobras
+tscv = TimeSeriesSplit(n_splits=n_splits)
+# Listas para armazenar os resultados de cada dobra
+train_scores, test_scores = [], []
+for train_index, test_index in tscv.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    # Construção do modelo LSTM
+    model = Sequential([
+        LSTM(50, input_shape=(X_train.shape[1], X_train.shape[2])),
+        Dense(1)
+    ])
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    # Treinamento do modelo
+    model.fit(X_train, y_train, epochs=50, batch_size=1, verbose=0)
+    # Avaliação do modelo
+    train_pred = model.predict(X_train)
+    test_pred = model.predict(X_test)
+    train_score = mean_squared_error(y_train, train_pred)
+    test_score = mean_squared_error(y_test, test_pred)
+    train_scores.append(train_score)
+    test_scores.append(test_score)
+    print(f"Train Score for fold {len(train_scores)}: {train_score}")
+    print(f"Test Score for fold {len(test_scores)}: {test_score}")
+# Média dos scores
+print(f"Average Train Score: {np.mean(train_scores)}")
+print(f"Average Test Score: {np.mean(test_scores)}")
+#----------------------------FIM - Validação Cruzada em Séries Temporais----------------------------#
+
+# Modelo LSTM
+model = Sequential([
+    InputLayer(input_shape=(look_back, 2)),  # Usando InputLayer para definir explicitamente a forma de entrada
+    LSTM(50),
+    Dense(1)
+])
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Treinamento do modelo
+model.fit(X, y, epochs=100, batch_size=1, verbose=1)
+
+# Previsões
+predictions = model.predict(X)
+predictions = scaler.inverse_transform(np.column_stack((predictions, np.zeros((predictions.shape[0], 1)))))
+
+# Últimos dados de look_back meses usados para fazer a primeira previsão futura
+last_samples = scaled_data[-look_back:]
+last_samples = np.expand_dims(last_samples, axis=0)
+
+# Gerando previsões futuras
+num_months = 3  # Exemplo: prever os próximos 6 meses
+future_predictions = []
+current_input = last_samples
+
+for _ in range(num_months):
+    next_prediction = model.predict(current_input)
+    future_predictions.append(next_prediction[0, 0])
+    next_prediction = np.expand_dims(next_prediction, axis=0)
+    next_prediction = np.repeat(next_prediction, 2, axis=2)  # Garantindo duas features
+    current_input = np.append(current_input[:, 1:, :], next_prediction, axis=1)
+
+future_predictions = np.array(future_predictions)
+future_predictions = scaler.inverse_transform(np.column_stack((future_predictions, np.zeros_like(future_predictions))))
+
+# Índices futuros para plotagem
+future_dates = pd.date_range(data.index[-1], periods=num_months + 1, freq='M')[1:]
+# Plotando os resultados
 plt.figure(figsize=(12, 6))
-plt.plot(data_2020.index, data_2020['Close'], label='Dados Históricos')
-plt.plot(extended_values.index, extended_values, color='red', label='Dados Ajustados e Previsões')
-plt.title('Previsões Futuras com Modelo ARIMA para os Próximos 30 Dias')
-plt.xlabel('Data')
-plt.ylabel('Close Price')
+plt.plot(data.index[look_back+1:], data['PETR4.SA'][look_back+1:], label='Original PETR4 Data')
+plt.plot(data.index[look_back+1:], predictions[:, 0], label='Predicted PETR4 Price', linestyle='-')
+plt.plot(future_dates, future_predictions[:, 0], label='Forecasted PETR4 Price', color='red')
+plt.plot(data.index, data['CL=F'], label='Oil Price', color='green')
+plt.gca().xaxis.set_major_formatter(DateFormatter('%Y-%m'))
+plt.gcf().autofmt_xdate()  # Auto-rotaciona as datas para melhor visualização
+plt.title('PETR4 Price Prediction using LSTM')
+plt.xlabel('Date')
+plt.ylabel('Price')
 plt.legend()
 plt.show()
-
-
-
-# #Definir o ticker da ação e o intervalo de datas
-# ticker = 'AAPL'
-# #Obter os dados
-# data = yf.download(ticker)
-# # Selecionar dados a partir de 2020
-# data_2020 = data['2020-01-01':]
-
-# # Ajustar o modelo ARIMA(2, 1, 2) aos seus dados
-# model = ARIMA(data_2020['Close'], order=(2, 1, 2))
-# results = model.fit()
-# # Prever os próximos 30 dias
-# forecast_values = results.forecast(steps=30)
-
-# # Criar datas para os próximos 30 dias
-# last_date = data_2020.index[-1]
-# next_dates = pd.date_range(start=last_date, periods=31)[1:]  # Ignorar o último dia dos dados históricos
-
-# # Plotar os dados originais e as previsões
-# plt.figure(figsize=(12, 6))
-# plt.plot(data_2020.index, data_2020['Close'], label='Dados Históricos')
-# plt.plot(next_dates, forecast_values, color='green', label='Previsões')
-# plt.plot(results.fittedvalues, color='red', label='ARIMA Values')
-# plt.title('Previsões Futuras com Modelo ARIMA para os Próximos 30 Dias')
-# plt.xlabel('Data')
-# plt.ylabel('Close Price')
-# plt.legend()
-# plt.show()
-
-
-#------------------------------------------------#
-
-# # Desativar os avisos (warnings) para manter a saída limpa
-# warnings.filterwarnings("ignore")
-
-# # Definir os valores possíveis para p, d e q
-# p_values = range(4)
-# d_values = [1]  # Já diferenciamos os dados uma vez
-# q_values = range(4)
-
-# # Criar uma lista de todas as combinações de p, d e q
-# pdq = list(itertools.product(p_values, d_values, q_values))
-
-# # Ajustar o modelo ARIMA para cada combinação de parâmetros
-# best_aic = np.inf
-# best_params = None
-
-# for param in pdq:
-#     try:
-#         # Ajustar o modelo ARIMA com os parâmetros dados
-#         model = ARIMA(data_2020['Close'], order=param)
-#         results = model.fit()
-
-#         # Calcular o AIC do modelo ajustado
-#         aic = results.aic
-
-#         # Atualizar o melhor modelo se o AIC for menor
-#         if aic < best_aic:
-#             best_aic = aic
-#             best_params = param
-
-#         print(f'ARIMA{param} - AIC: {aic}')
-#     except:
-#         continue
-
-# print(f'Best ARIMA Model: ARIMA{best_params} - AIC: {best_aic}')
-
-
-
-#------------------------------------------------#
-
-# # Diferenciar os dados para torná-los estacionários
-# diff_data = data_2020['Close'].diff().dropna()
-
-# # Plotar os dados diferenciados
-# plt.figure(figsize=(12, 6))
-# plt.plot(diff_data)
-# plt.title('Differenced Close Prices')
-# plt.xlabel('Date')
-# plt.ylabel('Price Difference')
-# plt.show()
-
-# # Verificar a autocorrelação dos dados diferenciados
-# plot_acf(diff_data, lags=20)
-# plt.title('Autocorrelation Function (ACF) of Differenced Close Prices')
-# plt.xlabel('Lag')
-# plt.ylabel('Autocorrelation')
-# plt.show()
-
-# plot_pacf(diff_data, lags=20)
-# plt.title('Partial Autocorrelation Function (PACF) of Differenced Close Prices')
-# plt.xlabel('Lag')
-# plt.ylabel('Partial Autocorrelation')
-# plt.show()
-
-
-
-#------------------------------------------------#
-
-# def calculate_sma(data, window=14):
-#     # Calcular a Média Móvel Simples (SMA)
-#     sma = data['Close'].rolling(window=window).mean()
-#     return sma
-
-# def calculate_rsi(data, window=14):
-#     # Calcular as mudanças diárias nos preços de fechamento
-#     delta = data['Close'].diff()
-
-#     # Separar as mudanças positivas e negativas
-#     gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
-#     loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
-
-#     # Calcular o RSI
-#     rs = gain / loss
-#     rsi = 100 - (100 / (1 + rs))
-
-#     return rsi
-
-# # Definir o valor da janela
-# window = 14
-
-# # Calcular a SMA para os dados históricos da ação da Apple
-# sma = calculate_sma(data.loc['2024-02-17':'2024-04-01'], window=window)
-
-# # Calcular o RSI para os dados históricos da ação da Apple
-# rsi = calculate_rsi(data.loc['2024-02-17':'2024-04-01'])
-
-# # Plotar os preços de fechamento, SMA e RSI
-# plt.figure(figsize=(12, 6))
-# # Plotar os preços de fechamento
-# plt.plot(data.loc['2024-02-17':'2024-04-01'].index, data.loc['2024-02-17':'2024-04-01']['Close'], label='Close Price', color='blue')
-# # Plotar o SMA
-# plt.plot(sma.index, sma, label=f'{window}-Day SMA', color='red')
-# # Plotar o RSI
-# plt.plot(rsi.index, rsi, label=f'{window}-Day RSI', color='green')
-# # Ajustar a escala dos eixos y para reduzir a distância entre as linhas
-# plt.ylim(data['Close'].min(), data['Close'].max())
-
-# plt.title('Close Price, SMA and RSI of AAPL')
-# plt.xlabel('Date')
-# plt.ylabel('Price / RSI')
-# plt.legend()
-# plt.show()
-
-
+#----------------------------FIM - Implementação Modelo LSTM----------------------------#
